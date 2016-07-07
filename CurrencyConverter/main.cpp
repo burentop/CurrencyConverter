dfe07de9709b4aabc5b8d8d14d8b7ebac80e2d36
Brent Perry <burentoperii@gmail.com>
2016-07-07 11:36:47 -0500

    Change to switch statement, and add yuan currency.

--- CurrencyConverter/main.cpp
+++ CurrencyConverter/main.cpp
@@ -13,20 +13,30 @@ int main()
     constexpr double yn_per_dollar = 0.0099;
     constexpr double kr_per_dollar = 0.15;
     constexpr double pd_per_dollar = 1.29;
+    constexpr double yu_per_dollar = 0.15;
     
     double amount = 1;
     
     char currency = ' ';
     
-    cout << "Please enter an amount followed by a currency (y, k or p - for 'yen', 'kroner' or 'pound'): ";
+    cout << "Please enter an amount followed by a currency (y, k, u or p - for 'yen', 'kroner', 'yuan' or 'pound'): ";
     cin >> amount >> currency;
     
-    if (currency == 'y')
-        cout << amount << "yen == " << yn_per_dollar*amount << "dollars\n";
-    else if (currency == 'k')
-        cout << amount << "kroner == " << kr_per_dollar*amount << "dollars\n";
-    else if (currency == 'p')
-        cout << amount << "pound == " << pd_per_dollar*amount << "dollars\n";
-    else
-        cout << "Sorry, I don't know a currency called '" << currency << "'\n";
+    switch (currency) {
+        case 'y':
+            cout << amount << "yen == " << yn_per_dollar*amount << "dollars\n";
+            break;
+        case 'k':
+            cout << amount << "kroner == " << kr_per_dollar*amount << "dollars\n";
+            break;
+        case 'p':
+            cout << amount << "pound == " << pd_per_dollar*amount << "dollars\n";
+            break;
+        case 'u':
+            cout << amount << "yuan == " << yu_per_dollar*amount << "dollars\n";
+            break;
+        default:
+            cout << "Sorry, I don't know a currency called '" << currency << "'\n";
+            break;
+    }
 }
