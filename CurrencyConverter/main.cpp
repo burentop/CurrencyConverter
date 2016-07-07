89707b2d75490794694ff020100659d3e6e13270
Brent Perry <burentoperii@gmail.com>
2016-07-07 11:07:17 -0500

    Initial commit of CurrencyConverter (as unit converter).

--- CurrencyConverter/main.cpp
+++ CurrencyConverter/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    constexpr double cm_per_inch = 2.54;
+    
+    double length = 1;
+    
+    char unit = ' ';
+    
+    cout << "Please enter a length followed by a unit (c or i): ";
+    cin >> length >> unit;
+    
+    if (unit == 'i')
+        cout << length << "in == " << cm_per_inch*length << "cm\n";
+    else if (unit == 'c')
+        cout << length << "cm == " << length/cm_per_inch << "in\n";
+    else
+        cout << "Sorry, I don't know a unit called '" << unit << "'\n";
 }
